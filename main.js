ff8b5c07cd67b2d926c7f9fb4c11c87accd1e46c
Harkness Wolf <ammophila.procera@gmail.com>
2020-11-16 02:48:17 -0600

    Added main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 600;
+canvas.height = 500;
+
+console.log(canvas);
+console.log(canvas.width);
